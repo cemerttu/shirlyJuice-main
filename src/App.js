246c24daef787a360e7813c753d886bdfcc37e7f
ian <cemenrttu@gmail.com>
2025-07-08 17:06:11 +0300

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HomeComponent from './components/homeComponet';
+
+function App() {
+  return (
+    <div>
+      < HomeComponent />
+    </div>
+  );
+}
+
+export default App;
